0cab0550537d5fc1c95494cc29e02b913f6f6077
Srinivas <122779862+roman-empire-ui@users.noreply.github.com>
2025-01-09 21:30:15 +0530

    last commit

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from 'path'
 import express from 'express';
 import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser';
@@ -14,16 +15,23 @@ dotenv.config()
 
 const port = process.env.PORT || 5000
 
+const __dirname = path.resolve()
+
 app.use(express.json())
 app.use(cookieParser())
 
 
-app.use('/api/auth' , authRoutes)
-app.use('/api/messages' , messageRoutes)
-app.use('/api/user' , userRoutes)
+app.use('/api/auth', authRoutes)
+app.use('/api/messages', messageRoutes)
+app.use('/api/user', userRoutes)
+
+app.use(express.static(path.join(__dirname, '/frontend/dist')))
+app.get('*' , (req , res) => {
+    res.sendFile(path.join(__dirname, 'frontend' , 'dist' , 'index.html'))
+})
 
 
-server.listen( port ,() => {
+server.listen(port, () => {
     connectDB()
     console.log(`Server is running on port ${port}`)
 })
